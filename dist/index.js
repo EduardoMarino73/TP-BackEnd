c627475da89a787462dfb62e6c32f25f6d1e611f
EduardoMarino73 <eduardoedumarino@gmail.com>
2026-04-27 20:21:42 -0300

    configure multer para que pueda guardar archivos en la memoria del equipo. Ademas logre levantar el servidor y enviar una respuesta a la pagina (creo) que dice hola mundo. Entiendo que estas son las 'puertas' de entrada y salida que voy a tener que usar

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,29 @@
-"use strict";
-console.log('go TS!');
+import express from "express";
+import multer from "multer";
+//levanto el Servidor   
+const app = express();
+app.listen(3000, () => {
+    console.log("El servidor esta iniciado ");
+});
+app.use('/', (req, res) => {
+    res.send('Hola Mundo');
+});
+//creo un objeto que sirve como configuracion para almacenar los datos en el disco
+const configuracion_almacenamiento = multer.diskStorage({
+    /*cuando hago este estilo de cosas, y por cosas me refiero a hacer "nombre de un atributo: function (...) {...}"
+    estoy diciendo que el valor de ese atributo se define cuando se resuelve el callback. Se supone que este metodo
+    me permite ser mas claro a la hora de pasarle los parametros con los que quiero formar ese atributo */
+    destination: function (req, file, cb) {
+        /* null significa que no hay error(?¿), mientras que './' indica la ruta en la que quiero que se guarde el
+        archivo */
+        cb(null, './');
+    },
+    filename: function (req, file, cb) {
+        /*algo similar a lo anterior solo que ahora le estoy nombre que quiero que tengan los archivos. Con esta
+        forma evito que se pisen los archivos cuando este haciendo pruebas */
+        cb(null, file.filename + ' ' + Date.now());
+    }
+});
+/*ahora le doy esa configuracion al objeto que voy a usar para guardar los archivos que reciba */
+const almacenamiento = multer({ storage: configuracion_almacenamiento });
 //# sourceMappingURL=index.js.map
