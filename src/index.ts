5a932a2fd102f6a46ce43d4192c41c485696d4c0
EduardoMarino73 <eduardoedumarino@gmail.com>
2026-05-05 18:34:19 -0300

    agregue la async function listarPeliculas para mostrar todas las peliculas guardadas en la carpeta correspondiente, falta hacer lo mismo para las series

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import cors from 'cors';
+import fs from "fs"
 
 //levanto el Servidor   
 const app = express()
@@ -8,11 +9,10 @@ app.listen(3000,() =>{
     console.log("El servidor esta iniciado ")
 })
 
-app.use(cors())
+/*objeto donde guardo las configuraciones que utilizo para enviar el contenido a travez de Express*/
+const configuracion_envio = {root : "./"}
 
-app.get('/',(req,res) =>{
-    res.send('Hola Mundo')
-})
+app.use(cors())
 
 //creo un objeto que sirve como configuracion para almacenar los datos en el disco
 const configuracion_almacenamiento = multer.diskStorage({
@@ -36,8 +36,37 @@ const configuracion_almacenamiento = multer.diskStorage({
 /*ahora le doy esa configuracion al objeto que voy a usar para guardar los archivos que reciba */
 const almacenamiento = multer({storage: configuracion_almacenamiento})
 
+/*el metodo "single" un unico archivo asociado a su campo que fue procesado por el middleware. 
+En caso de necesitar hacer esto con varios archivos al mismo tiempo tenemos que usar el metodo "fields"*/
 app.post('/api/videos',almacenamiento.single('contenido'),(req,res) =>{
     console.log(req.file?.originalname)
 
     res.send()
+})
+
+/*Siempre que se trabaje con archivos vamos a tener que usar la palabra async para indicar que trabajamos de forma asincrona.
+IMPORTANTE acordarse de poner esa palabra clave para no perder media tarde como me paso
+atte: Eduardo el 5/5/26 a las 18:17 */
+async function listarPeliculas() {
+
+    /*Creo un arreglo vacio donde guardo el nombre de las peliculas que encuentre*/
+    var peliculas:Array<String> = new Array<String>;
+
+    /*await indica que voy a esperar que lo que este enfrente termine.
+    La promesa me indica que quizas ahora no tenga lo que estoy buscando pero que mas adelante me lo va a entregar */
+    const directorio = await fs.promises.opendir("./src/contenido/peliculas");
+
+    /*Recorro el directorio agregando los nombre de las peliculas al arreglo de peliculas */
+    for await(const contenido of directorio){
+        peliculas.push(contenido.name);
+    }
+
+    /*Devuelvo el arreglo con todas las peliculas que encontre(puede llegar a estar vacio) */
+    return peliculas
+}
+
+/*Como estamos enviando el resultado de una funcion funcion asincrona hacemos la arrow function asincrona tambien.
+Esto es para evitar tener que manejar la promesa que nos devolveria con el metodo then(parametro => {...}) */
+app.get('/contenido/peliculas',async (req,res) => {
+    res.send(await listarPeliculas());
 })
