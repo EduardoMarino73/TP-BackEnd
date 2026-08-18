3cba7d3e72c530ff968a9ca1ff8c94aec93ae977
EduardoMarino73 <eduardoedumarino@gmail.com>
2026-08-17 22:19:30 -0300

    olvide agregar content.Storage al ultimo commit

--- src/Shared/database/content/content.Storage.ts
+++ src/Shared/database/content/content.Storage.ts
@@ -1,6 +1,7 @@
 import { Request } from "express";
 import path from "path"
 
+/**the path where save movies */
 export const moviePath = "src\\Shared\\database\\content\\movies";
 
 export const movieTittle = (req:Request,file:string): string => {
